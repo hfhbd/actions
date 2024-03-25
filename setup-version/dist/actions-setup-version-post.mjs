b6c82606743b70caedf473d167dc87c2f5835cee
hfhbd <hfhbd@users.noreply.github.com>
2024-03-25 17:49:18 +0100

    Use es modules

---
+++ setup-version/dist/actions-setup-version-post.mjs
@@ -0,0 +1,12 @@
+import { println2shhhgwwt4c61 as println } from './kotlin-kotlin-stdlib.mjs';
+//region block: imports
+//endregion
+//region block: pre-declaration
+//endregion
+function main() {
+  println('Hello Post');
+}
+function mainWrapper() {
+  main();
+}
+mainWrapper();
