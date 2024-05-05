f2b0d4fe1be9bf33fffa4c8f75db91fce574ef4a
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-05-05 16:55:26 +0200

    Return version as output

--- setup-version/src/mainMain/kotlin/action.kt
+++ setup-version/src/mainMain/kotlin/action.kt
@@ -1,4 +1,3 @@
-import actions.core.exportVariable
 import com.github.actions.*
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -9,35 +8,42 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
-suspend fun action(token: String?): Outputs {
-    val (ref, version) = when (val event = github.context.eventName) {
-        "release" -> {
-            val ref = github.context.ref
-            val version = ref.removePrefix("refs/tags/v")
-            ref to version
-        }
-
-        "workflow_dispatch" -> {
-            val ref = github.context.ref
-            val version = if (ref.startsWith("refs/heads/main")) {
-                getLatestVersion(github.context.repo.owner, github.context.repo.repo, token ?: github.context.token)
-            } else if (ref.startsWith("refs/tags/v")){
-                ref.removePrefix("refs/tags/v")
-            } else error("Not supported ref: $ref")
-            ref to "$version.${github.context.runNumber}"
-        }
-
-        "schedule" -> {
-            val latestVersion = getLatestVersion(github.context.repo.owner, github.context.repo.repo, token ?: github.context.token)
-            "refs/tags/v$latestVersion" to "$latestVersion.${github.context.runNumber}"
-        }
-
-        else -> error("Not supported event: $event")
+suspend fun action(token: String?): Outputs = when (val event = github.context.eventName) {
+    "release" -> {
+        val ref = github.context.ref
+        Outputs(ref = ref, version = ref.removePrefix("refs/tags/v"))
     }
-    exportVariable("version", version)
-    return Outputs(
-        ref = ref,
-    )
+
+    "workflow_dispatch" -> {
+        val ref = github.context.ref
+        val version = if (ref.startsWith("refs/heads/main")) {
+            getLatestVersion(
+                owner = github.context.repo.owner,
+                repo = github.context.repo.repo,
+                token = token ?: github.context.token,
+            )
+        } else if (ref.startsWith("refs/tags/v")) {
+            ref.removePrefix("refs/tags/v")
+        } else error("Not supported ref: $ref")
+        Outputs(
+            ref = ref,
+            version = "$version.${github.context.runNumber}",
+        )
+    }
+
+    "schedule" -> {
+        val latestVersion = getLatestVersion(
+            owner = github.context.repo.owner,
+            repo = github.context.repo.repo,
+            token = token ?: github.context.token,
+        )
+        Outputs(
+            ref = "refs/tags/v$latestVersion",
+            version = "$latestVersion.${github.context.runNumber}"
+        )
+    }
+
+    else -> error("Not supported event: $event")
 }
 
 suspend fun getLatestVersion(
